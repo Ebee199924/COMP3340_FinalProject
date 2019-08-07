432470c6dd94b125cf5f0f634e743b29ebfad369
Ebee199924 <53879991+Ebee199924@users.noreply.github.com>
2019-08-07 17:21:58 -0400

    Create return-policy.php

---
+++ php/return-policy.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Windsor Public Library</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="theme-color" content="#03a6f3">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:200,300,400,500,600,700,800,900" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+    <?php include "header.php"; ?>
+    <div class="breadcrumb">
+        <div class="container">
+            <a class="breadcrumb-item" href="index.php">Home</a>
+            <span class="breadcrumb-item active">Shipping & Returns</span>
+        </div>
+    </div>
+    <section class="static about-sec">
+        <div class="container">
+            <h1>Return Policy</h1>
+            <strong>Late return charges</strong>
+            <p>When returning a borrowed item to the library later than the borrowing deadline, you will receive a fine of $5 which increases by $1 for every day the item is not returned, with a cap of $25 in fees</p>
+            <strong>Exceptions</strong>
+            <p>Exceptions can be made with the proper reasoning such as having been incapable to return by the deadline due to family emergencies, health complications etc. Children under the age of 10 who borrow items from the library are also exempt from any late return fees</p>
+            </div>
+    </section>
+    <?php include "footer.php"; ?>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/owl.carousel.min.js"></script>
+    <script src="js/custom.js"></script>
+</body>
+
+</html>
