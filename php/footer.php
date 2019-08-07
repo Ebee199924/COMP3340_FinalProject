0c1fb07918941954d2b9e1f56e29d0725fbed7fb
Ebee199924 <53879991+Ebee199924@users.noreply.github.com>
2019-08-07 17:18:43 -0400

    Create footer.php

---
+++ php/footer.php
@@ -0,0 +1,70 @@
+<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="address">
+                        <h4>Our Address</h4>
+                        <h6>Windsor Public Library, Devonshire Mall,
+                        3100 Howard Ave, Windsor, ON N8X 3Y8</h6>
+                        <h6>Call : 800 1234 5678</h6>
+                        <h6>Email : WinPubLib@gmail.com</h6>
+                    </div>
+                    <div class="timing">
+                        <h4>Working Hours</h4>
+                        <h6>Mon - Sat: 7am - 10pm</h6>
+                        <h6>Sunday: 8am - 10pm</h6>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="navigation">
+                        <h4>Navigation</h4>
+                        <ul>
+                            <li><a href="index.php">Home</a></li>
+                            <li><a href="about.php">About Us</a></li>
+                            <li><a href="privacy-policy.php">Privacy Policy</a></li>
+                            <li><a href="terms-conditions.php">Terms</a></li>
+                            <li><a href="products.php">Browse</a></li>
+                        </ul>
+                    </div>
+                    <div class="navigation">
+                        <h4>Help</h4>
+                        <ul>
+                            <li><a href="return-policy.php">Late Returns</a></li>
+                            <li><a href="privacy-policy.php">Privacy</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-5">
+                    <div class="form">
+                        <h3>Contact us</h3>
+                        <h6>Help us help you</h6>
+                        <form>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <input placeholder="Name" required>
+                                </div>
+                                <div class="col-md-6">
+                                    <input type="email" placeholder="Email" required>
+                                </div>
+                                <div class="col-md-12">
+                                    <textarea placeholder="Message"></textarea>
+                                </div>
+                                <div class="col-md-12">
+                                    <button class="btn black">Submit</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="copy-right">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h5>(C) 2019. All Rights Reserved. Windsor Public Library</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
