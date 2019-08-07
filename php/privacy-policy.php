c8d3efcb67850d35456e5dc5b8ef0fe234ee846b
Ebee199924 <53879991+Ebee199924@users.noreply.github.com>
2019-08-07 17:20:57 -0400

    Update privacy-policy.php

--- php/privacy-policy.php
+++ php/privacy-policy.php
@@ -1,1 +1,38 @@
-https://myweb.cs.uwindsor.ca:2222/CMD_FILE_MANAGER/domains/kenne%31%32%37.myweb.cs.uwindsor.ca/public%5Fhtml/%36%30%33%33%34/project/privacy%2Dpolicy%2Ephp?action=edit
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Windsor Public Library</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="theme-color" content="#03a6f3">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:200,300,400,500,600,700,800,900" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+    <?php include "header.php"; ?>
+    <div class="breadcrumb">
+        <div class="container">
+            <a class="breadcrumb-item" href="index.php">Home</a>
+            <span class="breadcrumb-item active">Privacy Policy</span>
+        </div>
+    </div>
+    <section class="static about-sec">
+        <div class="container">
+            <h1>Privacy Policy</h1>
+            <p>The Windsor Public Library Board will make information about the Library available to the public, and will protect the privacy of all personal information in its custody in keeping with the access and privacy provisions of MFIPPA and other applicable legislation.</p>
+            <p><b><a href=" http://www.e-laws.gov.on.ca/html/statutes/english/elaws_statutes_90m56_e.htm">Municipal Freedom of Information and Protection of Privacy Act</a></b></p>
+        </div>
+    </section>
+    <?php include "footer.php"; ?>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/owl.carousel.min.js"></script>
+    <script src="js/custom.js"></script>
+</body>
+
+</html>
