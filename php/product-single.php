20259dcc0d646a2c5d4cb83d50365ecad6d438af
Ebee199924 <53879991+Ebee199924@users.noreply.github.com>
2019-08-07 17:20:27 -0400

    Update product-single.php

--- php/product-single.php
+++ php/product-single.php
@@ -1,1 +1,134 @@
-https://myweb.cs.uwindsor.ca:2222/CMD_FILE_MANAGER/domains/kenne%31%32%37.myweb.cs.uwindsor.ca/public%5Fhtml/%36%30%33%33%34/project/product%2Dsingle%2Ephp?action=edit
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Windsor Public Library</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="theme-color" content="#03a6f3">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:200,300,400,500,600,700,800,900" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+    <?php include "header.php"; ?>
+    <div class="breadcrumb">
+        <div class="container">
+            <a class="breadcrumb-item" href="index.php">Home</a>
+            <span class="breadcrumb-item active">Terms and Condition</span>
+        </div>
+    </div>
+    <section class="product-sec">
+        <div class="container">
+            <h1>7 Day Self publish How to Write a Book</h1>
+            <div class="row">
+                <div class="col-md-6 slider-sec">
+                    <!-- main slider carousel -->
+                    <div id="myCarousel" class="carousel slide">
+                        <!-- main slider carousel items -->
+                        <div class="carousel-inner">
+                            <div class="active item carousel-item" data-slide-number="0">
+                                <img src="images/product1.jpg" class="img-fluid">
+                            </div>
+                            <div class="item carousel-item" data-slide-number="1">
+                                <img src="images/product2.jpg" class="img-fluid">
+                            </div>
+                            <div class="item carousel-item" data-slide-number="2">
+                                <img src="images/product3.jpg" class="img-fluid">
+                            </div>
+                        </div>
+                        <!-- main slider carousel nav controls -->
+                        <ul class="carousel-indicators list-inline">
+                            <li class="list-inline-item active">
+                                <a id="carousel-selector-0" class="selected" data-slide-to="0" data-target="#myCarousel">
+                                <img src="images/product1.jpg" class="img-fluid">
+                            </a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a id="carousel-selector-1" data-slide-to="1" data-target="#myCarousel">
+                                <img src="images/product2.jpg" class="img-fluid">
+                            </a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a id="carousel-selector-2" data-slide-to="2" data-target="#myCarousel">
+                                <img src="images/product3.jpg" class="img-fluid">
+                            </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!--/main slider carousel-->
+                </div>
+                <div class="col-md-6 slider-content">
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's printer took a galley of type and Scrambled it to make a type and typesetting industry. Lorem Ipsum has been the book. </p>
+                    <p>t has survived not only fiveLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's printer took a galley of type and</p>
+                    <div class="btn-sec">
+                        <button class="btn black">Reserve Now</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="related-books">
+        <div class="container">
+            <h2>You may also like these book</h2>
+            <div class="recomended-sec">
+                <div class="row">
+                    <div class="col-lg-3 col-md-6">
+                    <div class="item">
+                        <img src="images/img1.jpg" alt="img">
+                        <h3>how to be a bwase</h3>
+                        <div class="hover">
+                            <a href="product-single.php">
+                            <span><i class="fa fa-long-arrow-right" aria-hidden="true"></i></span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                    <div class="col-lg-3 col-md-6">
+                    <div class="item">
+                        <img src="images/img1.jpg" alt="img">
+                        <h3>how to be a bwase</h3>
+                        <div class="hover">
+                            <a href="product-single.php">
+                            <span><i class="fa fa-long-arrow-right" aria-hidden="true"></i></span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                    <div class="col-lg-3 col-md-6">
+                    <div class="item">
+                        <img src="images/img1.jpg" alt="img">
+                        <h3>how to be a bwase</h3>
+                        <div class="hover">
+                            <a href="product-single.php">
+                            <span><i class="fa fa-long-arrow-right" aria-hidden="true"></i></span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                    <div class="col-lg-3 col-md-6">
+                    <div class="item">
+                        <img src="images/img1.jpg" alt="img">
+                        <h3>how to be a bwase</h3>
+                        <div class="hover">
+                            <a href="product-single.php">
+                            <span><i class="fa fa-long-arrow-right" aria-hidden="true"></i></span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <?php include "footer.php"; ?>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/owl.carousel.min.js"></script>
+    <script src="js/custom.js"></script>
+</body>
+
+</html>
