31e5f098bd2a80447b46f7ed7b97754a2d9222c2
Ebee199924 <53879991+Ebee199924@users.noreply.github.com>
2019-08-07 17:21:23 -0400

    Create products.php

---
+++ php/products.php
@@ -0,0 +1,294 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Windsor Public Library</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="theme-color" content="#03a6f3">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:200,300,400,500,600,700,800,900" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+    <?php include "header.php"; ?>
+    <div class="breadcrumb">
+        <div class="container">
+            <a class="breadcrumb-item" href="index.php">Home</a>
+            <span class="breadcrumb-item active">Products</span>
+        </div>
+    </div>
+    <section class="static about-sec">
+        <div class="container">
+            <h2>Popular Picks</h2>
+            <div class="recomended-sec">
+                <div class="row">
+                    <div class="col-lg-3 col-md-6">
+                    <div class="item">
+                        <img src="images/img1.jpg" alt="img">
+                        <h3>how to be a bwase</h3>
+                        <div class="hover">
+                            <a href="product-single.php">
+                            <span><i class="fa fa-long-arrow-right" aria-hidden="true"></i></span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                    <div class="col-lg-3 col-md-6">
+                    <div class="item">
+                        <img src="images/img1.jpg" alt="img">
+                        <h3>how to be a bwase</h3>
+                        <div class="hover">
+                            <a href="product-single.php">
+                            <span><i class="fa fa-long-arrow-right" aria-hidden="true"></i></span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                    <div class="col-lg-3 col-md-6">
+                    <div class="item">
+                        <img src="images/img1.jpg" alt="img">
+                        <h3>how to be a bwase</h3>
+                        <div class="hover">
+                            <a href="product-single.php">
+                            <span><i class="fa fa-long-arrow-right" aria-hidden="true"></i></span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                    <div class="col-lg-3 col-md-6">
+                    <div class="item">
+                        <img src="images/img1.jpg" alt="img">
+                        <h3>how to be a bwase</h3>
+                        <div class="hover">
+                            <a href="product-single.php">
+                            <span><i class="fa fa-long-arrow-right" aria-hidden="true"></i></span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            <h2>fantasy</h2>
+            <div class="recent-book-sec">
+                <div class="row">
+                    <div class="col-lg-2 col-md-3 col-sm-4">
+                        <div class="item">
+                            <img src="images/r1.jpg" alt="img">
+                            <h3><a href="product-single.php">Keepers of the kalachakara</a></h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-3 col-sm-4">
+                        <div class="item">
+                            <img src="images/r1.jpg" alt="img">
+                            <h3><a href="product-single.php">Keepers of the kalachakara</a></h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-3 col-sm-4">
+                        <div class="item">
+                            <img src="images/r1.jpg" alt="img">
+                            <h3><a href="product-single.php">Keepers of the kalachakara</a></h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-3 col-sm-4">
+                        <div class="item">
+                            <img src="images/r1.jpg" alt="img">
+                            <h3><a href="product-single.php">Keepers of the kalachakara</a></h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-3 col-sm-4">
+                        <div class="item">
+                            <img src="images/r1.jpg" alt="img">
+                            <h3><a href="product-single.php">Keepers of the kalachakara</a></h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <h2>horror</h2>
+            <div class="recent-book-sec">
+                <div class="row">
+                    <div class="col-lg-2 col-md-3 col-sm-4">
+                        <div class="item">
+                            <img src="images/r1.jpg" alt="img">
+                            <h3><a href="product-single.php">Keepers of the kalachakara</a></h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-3 col-sm-4">
+                        <div class="item">
+                            <img src="images/r1.jpg" alt="img">
+                            <h3><a href="product-single.php">Keepers of the kalachakara</a></h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-3 col-sm-4">
+                        <div class="item">
+                            <img src="images/r1.jpg" alt="img">
+                            <h3><a href="product-single.php">Keepers of the kalachakara</a></h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-3 col-sm-4">
+                        <div class="item">
+                            <img src="images/r1.jpg" alt="img">
+                            <h3><a href="product-single.php">Keepers of the kalachakara</a></h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-3 col-sm-4">
+                        <div class="item">
+                            <img src="images/r1.jpg" alt="img">
+                            <h3><a href="product-single.php">Keepers of the kalachakara</a></h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <h2>mystery</h2>
+            <div class="recent-book-sec">
+                <div class="row">
+                    <div class="col-lg-2 col-md-3 col-sm-4">
+                        <div class="item">
+                            <img src="images/r1.jpg" alt="img">
+                            <h3><a href="product-single.php">Keepers of the kalachakara</a></h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-3 col-sm-4">
+                        <div class="item">
+                            <img src="images/r1.jpg" alt="img">
+                            <h3><a href="product-single.php">Keepers of the kalachakara</a></h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-3 col-sm-4">
+                        <div class="item">
+                            <img src="images/r1.jpg" alt="img">
+                            <h3><a href="product-single.php">Keepers of the kalachakara</a></h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-3 col-sm-4">
+                        <div class="item">
+                            <img src="images/r1.jpg" alt="img">
+                            <h3><a href="product-single.php">Keepers of the kalachakara</a></h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-3 col-sm-4">
+                        <div class="item">
+                            <img src="images/r1.jpg" alt="img">
+                            <h3><a href="product-single.php">Keepers of the kalachakara</a></h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <h2>for kids</h2>
+            <div class="recent-book-sec">
+                <div class="row">
+                    <div class="col-lg-2 col-md-3 col-sm-4">
+                        <div class="item">
+                            <img src="images/r1.jpg" alt="img">
+                            <h3><a href="product-single.php">Keepers of the kalachakara</a></h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-3 col-sm-4">
+                        <div class="item">
+                            <img src="images/r1.jpg" alt="img">
+                            <h3><a href="product-single.php">Keepers of the kalachakara</a></h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-3 col-sm-4">
+                        <div class="item">
+                            <img src="images/r1.jpg" alt="img">
+                            <h3><a href="product-single.php">Keepers of the kalachakara</a></h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-3 col-sm-4">
+                        <div class="item">
+                            <img src="images/r1.jpg" alt="img">
+                            <h3><a href="product-single.php">Keepers of the kalachakara</a></h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-3 col-sm-4">
+                        <div class="item">
+                            <img src="images/r1.jpg" alt="img">
+                            <h3><a href="product-single.php">Keepers of the kalachakara</a></h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <h2>Textbooks</h2>
+            <div class="recent-book-sec">
+                <div class="row">
+                    <div class="col-lg-2 col-md-3 col-sm-4">
+                        <div class="item">
+                            <img src="images/r1.jpg" alt="img">
+                            <h3><a href="product-single.php">Keepers of the kalachakara</a></h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-3 col-sm-4">
+                        <div class="item">
+                            <img src="images/r1.jpg" alt="img">
+                            <h3><a href="product-single.php">Keepers of the kalachakara</a></h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-3 col-sm-4">
+                        <div class="item">
+                            <img src="images/r1.jpg" alt="img">
+                            <h3><a href="product-single.php">Keepers of the kalachakara</a></h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-3 col-sm-4">
+                        <div class="item">
+                            <img src="images/r1.jpg" alt="img">
+                            <h3><a href="product-single.php">Keepers of the kalachakara</a></h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-3 col-sm-4">
+                        <div class="item">
+                            <img src="images/r1.jpg" alt="img">
+                            <h3><a href="product-single.php">Keepers of the kalachakara</a></h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <h2>Science Fiction</h2>
+            <div class="recent-book-sec">
+                <div class="row">
+                    <div class="col-lg-2 col-md-3 col-sm-4">
+                        <div class="item">
+                            <img src="images/r1.jpg" alt="img">
+                            <h3><a href="product-single.php">Keepers of the kalachakara</a></h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-3 col-sm-4">
+                        <div class="item">
+                            <img src="images/r1.jpg" alt="img">
+                            <h3><a href="product-single.php">Keepers of the kalachakara</a></h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-3 col-sm-4">
+                        <div class="item">
+                            <img src="images/r1.jpg" alt="img">
+                            <h3><a href="product-single.php">Keepers of the kalachakara</a></h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-3 col-sm-4">
+                        <div class="item">
+                            <img src="images/r1.jpg" alt="img">
+                            <h3><a href="product-single.php">Keepers of the kalachakara</a></h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-3 col-sm-4">
+                        <div class="item">
+                            <img src="images/r1.jpg" alt="img">
+                            <h3><a href="product-single.php">Keepers of the kalachakara</a></h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <?php include "footer.php"; ?>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/owl.carousel.min.js"></script>
+    <script src="js/custom.js"></script>
+</body>
+
+</html>
