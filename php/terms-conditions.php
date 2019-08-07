42bbb4032ad960091890dc0cb9cbda7d3f209618
Ebee199924 <53879991+Ebee199924@users.noreply.github.com>
2019-08-07 17:22:10 -0400

    Create terms-conditions.php

---
+++ php/terms-conditions.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Windsor Public Library</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="theme-color" content="#03a6f3">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:200,300,400,500,600,700,800,900" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+    <?php include "header.php"; ?>
+    <div class="breadcrumb">
+        <div class="container">
+            <a class="breadcrumb-item" href="index.php">Home</a>
+            <span class="breadcrumb-item active">Terms and Condition</span>
+        </div>
+    </div>
+    <section class="static about-sec">
+        <div class="container">
+            <h1>TErms and Condition</h1>
+            <h3>COPYRIGHT</h3>
+            <p>All information on or accessed through this website, including but not limited to text, graphics, source codes, photographs, trademarks, and logos, is protected under Canadian and international copyright laws.</p>
+            <p>No one may copy, redistribute, reproduce, or republish information found on this website or on pages accessed through this website for non-personal use without advance authorization.</p>
+            <h3>Committment to Privacy</h3>
+            <p>Windsor Public Library is deeply committed to ensuring the privacy of the information given to us by individuals wishing to use the services of the library.</p>
+            <p>The library collects this information in accordance with the Municipal Freedom of Information and Protection of Privacy Act (MFIPPA) and under the authority of the Public Libraries Act in order to conduct library operations.</p>
+    </div>
+    </section>
+    <?php include "footer.php"; ?>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/owl.carousel.min.js"></script>
+    <script src="js/custom.js"></script>
+</body>
+
+</html>
