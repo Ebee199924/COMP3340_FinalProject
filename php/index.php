d735d377731f86d7488d89088a2e8b0380803890
Ebee199924 <53879991+Ebee199924@users.noreply.github.com>
2019-08-07 17:19:14 -0400

    Create index.php

---
+++ php/index.php
@@ -0,0 +1,227 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Windsor Public Library</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="theme-color" content="#03a6f3">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:200,300,400,500,600,700,800,900" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+    <?php include "header.php"; ?>
+    <section class="slider">
+        <div class="container">
+            <div id="owl-demo" class="owl-carousel owl-theme">
+                <div class="item">
+                    <div class="slide">
+                        <img src="images/slide1.jpg" alt="slide1">
+                        <div class="content">
+                            <div class="title">
+                                <h3>Welcome to the Windsor Public Library</h3>
+                                <h5>browse through a wide variety of genres</h5>
+                                <a href="products.php" class="btn">browse books</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="slide">
+                        <img src="images/slide2.jpg" alt="slide1">
+                        <div class="content">
+                            <div class="title">
+                                <h3>Welcome to the Windsor Public Library</h3>
+                                <h5>borrow from our selection of films</h5>
+                                <a href="products.php" class="btn">browse films</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="recomended-sec">
+        <div class="container">
+            <div class="title">
+                <h2>Staff Picks</h2>
+                <hr>
+            </div>
+            <div class="row">
+                <div class="col-lg-3 col-md-6">
+                    <div class="item">
+                        <img src="images/img1.jpg" alt="img">
+                        <h3>how to be a bwase</h3>
+                        <div class="hover">
+                            <a href="product-single.php">
+                            <span><i class="fa fa-long-arrow-right" aria-hidden="true"></i></span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="item">
+                        <img src="images/img1.jpg" alt="img">
+                        <h3>how to be a bwase</h3>
+                        <div class="hover">
+                            <a href="product-single.php">
+                            <span><i class="fa fa-long-arrow-right" aria-hidden="true"></i></span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="item">
+                        <img src="images/img1.jpg" alt="img">
+                        <h3>how to be a bwase</h3>
+                        <div class="hover">
+                            <a href="product-single.php">
+                            <span><i class="fa fa-long-arrow-right" aria-hidden="true"></i></span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="item">
+                        <img src="images/img1.jpg" alt="img">
+                        <h3>how to be a bwase</h3>
+                        <div class="hover">
+                            <a href="product-single.php">
+                            <span><i class="fa fa-long-arrow-right" aria-hidden="true"></i></span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="recent-book-sec">
+        <div class="container">
+            <div class="title">
+                <h2>Trending right now</h2>
+                <hr>
+            </div>
+            <div class="row">
+                <div class="col-lg-2 col-md-3 col-sm-4">
+                        <div class="item">
+                            <img src="images/r1.jpg" alt="img">
+                            <h3><a href="product-single.php">Keepers of the kalachakara</a></h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-3 col-sm-4">
+                        <div class="item">
+                            <img src="images/r1.jpg" alt="img">
+                            <h3><a href="product-single.php">Keepers of the kalachakara</a></h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-3 col-sm-4">
+                        <div class="item">
+                            <img src="images/r1.jpg" alt="img">
+                            <h3><a href="product-single.php">Keepers of the kalachakara</a></h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-3 col-sm-4">
+                        <div class="item">
+                            <img src="images/r1.jpg" alt="img">
+                            <h3><a href="product-single.php">Keepers of the kalachakara</a></h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-3 col-sm-4">
+                        <div class="item">
+                            <img src="images/r1.jpg" alt="img">
+                            <h3><a href="product-single.php">Keepers of the kalachakara</a></h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-3 col-sm-4">
+                        <div class="item">
+                            <img src="images/r1.jpg" alt="img">
+                            <h3><a href="product-single.php">Keepers of the kalachakara</a></h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-3 col-sm-4">
+                        <div class="item">
+                            <img src="images/r1.jpg" alt="img">
+                            <h3><a href="product-single.php">Keepers of the kalachakara</a></h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-3 col-sm-4">
+                        <div class="item">
+                            <img src="images/r1.jpg" alt="img">
+                            <h3><a href="product-single.php">Keepers of the kalachakara</a></h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-3 col-sm-4">
+                        <div class="item">
+                            <img src="images/r1.jpg" alt="img">
+                            <h3><a href="product-single.php">Keepers of the kalachakara</a></h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-3 col-sm-4">
+                        <div class="item">
+                            <img src="images/r1.jpg" alt="img">
+                            <h3><a href="product-single.php">Keepers of the kalachakara</a></h3>
+                        </div>
+                    </div>
+            </div>
+            <div class="btn-sec">
+                <a href="products.php" class="btn gray-btn">view all books</a>
+            </div>
+        </div>
+    </section>
+    <section class="features-sec">
+        <div class="container">
+            <ul>
+                <li>
+                    <span class="icon"><i class="fa fa-users" aria-hidden="true"></i></span>
+                    <h3>LOYALTY BENEFITS</h3>
+                    <h5>Borrowing limit increased</h5>
+                    <h6>Customers Registered at the Windsor Public Library for over a year can borrow any 10 books at once compared to the regular 5 books</h6>
+                </li>
+                <li>
+                    <span class="icon return"><i class="fa fa-book" aria-hidden="true"></i></span>
+                    <h3>TEACHER PRIVILEGES</h3>
+                    <h5>Extensions for education</h5>
+                    <h6>Any teachers registered at the Windsor Public Library can borrow books for up to 4 months compared to the regular single month</h6>
+                </li>
+                <li>
+                    <span class="icon chat"><i class="fa fa-comments" aria-hidden="true"></i></span>
+                    <h3>ONLINE SUPPORT</h3>
+                    <h5>Helpful customer service</h5>
+                    <h6>Customer support tickets and inquiries available online at all times to suit your needs at any hour</h6>
+                </li>
+            </ul>
+        </div>
+    </section>
+    <section class="offers-sec" style="background:url(images/offers.jpg)no-repeat;">
+        <div class="cover"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="detail">
+                        <h3>Largest Selection in Windsor-Essex</h3>
+                        <h6>With a selection this wide you're bound to find exactly what you're looking for!</h6>
+                        <a href="products.php" class="btn blue-btn">view all books</a>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="detail">
+                        <h3>Wide Range of Genres</h3>
+                        <h6>Nearly every genre and subgenre can be found at our libraries so take a look!</h6>
+                        <a href="products.php" class="btn blue-btn">view all books</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <?php include "footer.php"; ?>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/owl.carousel.min.js"></script>
+    <script src="js/custom.js"></script>
+</body>
+
+</html>
