dc6a2f27059694a9adce3d798ff7c22228e76f7a
Ebee199924 <53879991+Ebee199924@users.noreply.github.com>
2019-08-07 17:18:58 -0400

    Create header.php

---
+++ php/header.php
@@ -0,0 +1,44 @@
+<header>
+        <div class="header-top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3"><a href="index.php" class="web-url">www.windsorpubliclibrary.com</a></div>
+                    <div class="col-md-6">
+                        <h5>Free Library Card Renewals for Members of Over Two Years</h5></div>
+                    <div class="col-md-3">
+                        <span class="ph-number">Call : 800 1234 5678</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="main-menu">
+            <div class="container">
+                <nav class="navbar navbar-expand-lg navbar-light">
+                    <a class="navbar-brand" href="index.php"><img src="images/logo.png" alt="logo"></a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ml-auto">
+                            <li class="navbar-item active">
+                                <a href="index.php" class="nav-link">Home</a>
+                            </li>
+                            <li class="navbar-item">
+                                <a href="products.php" class="nav-link">Browse</a>
+                            </li>
+                            <li class="navbar-item">
+                                <a href="about.php" class="nav-link">About</a>
+                            </li>
+                            <li class="navbar-item">
+                                <a href="login.php" class="nav-link">Login</a>
+                            </li>
+                        </ul>
+                        <form class="form-inline my-2 my-lg-0">
+                            <input class="form-control mr-sm-2" type="search" placeholder="Search here..." aria-label="Search">
+                            <span class="fa fa-search"></span>
+                        </form>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </header>
