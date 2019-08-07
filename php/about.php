b041929bfd045a8e0d6351f4a069ecc7a9b85fce
Ebee199924 <53879991+Ebee199924@users.noreply.github.com>
2019-08-07 17:18:02 -0400

    Create about.php

---
+++ php/about.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Windsor Public Library</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="theme-color" content="#03a6f3">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:200,300,400,500,600,700,800,900" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+    <?php include "header.php"; ?>
+    <div class="breadcrumb">
+        <div class="container">
+            <a class="breadcrumb-item" href="index.php">Home</a>
+            <span class="breadcrumb-item active">About</span>
+        </div>
+    </div>
+    <section class="static about-sec">
+        <div class="container">
+            <h1>About Us</h1>
+            <div class="img-sec">
+                <img src="images/about.jpg" alt="about">
+                <p>Windsor Public Library has been providing book lending services to the Windsor-Essex region for over 1 week. With such a long history of involvement in the local community you'll always feel comfortable and at home browsing through our selection of literature ranging from books for kids to timeless classics.</p>
+                <p>Our priority is and will always be to put the customer experience first. Our employees are always ready to assist you with any problems you may have such as locating a book or using our computers if needed. Our Librarians are friendly and love nothing more than to help you find a new book to love!</p>
+            </div>
+        </div>
+    </section>
+    <?php include "footer.php"; ?>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/owl.carousel.min.js"></script>
+    <script src="js/custom.js"></script>
+</body>
+
+</html>
